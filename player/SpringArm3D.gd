86f4e709940a9f40936ecdc3d7581ffac7da9c23
Javi Penaloza <veralece@gmail.com>
2024-08-19 18:07:42 -0700

    rat can walk and waddle!

---
+++ player/SpringArm3D.gd
@@ -0,0 +1,15 @@
+extends SpringArm3D
+
+@export var mouse_sensitivity = 0.05
+
+func _ready():
+	set_as_top_level(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.x = clamp(rotation.x, -90.0, 30.0)
+		
+		rotation.y -= event.relative.x * mouse_sensitivity
+		rotation.y = wrapf(rotation.y, 0.0, 360.0)
--- player/player.gd
+++ player/player.gd
@@ -4,31 +4,28 @@ extends CharacterBody3D
 @export var jump_impulse = 20.0
 @export var gravity = 75.0
 @export var bounce_impulse = 16.0
-
+@export var rotation_speed = 5.0
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_backward"):
-		direction.z += 1
-	
-	if Input.is_action_pressed("jump") && is_on_floor():
-		velocity.y += jump_impulse
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	var is_jumping = Input.is_action_just_pressed("jump") && is_on_floor()
 	
 	#check to see if direction was modified on this frame, then normalize it.
 	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		%Rat.look_at(position + direction, Vector3.UP)
+		direction = direction.rotated(Vector3.UP, %SpringArm3D.rotation.y).normalized()
 	
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
-	#velocity.y -= gravity * delta
+	velocity.y -= gravity * delta
+	
+	if is_jumping:
+		velocity.y += jump_impulse
+	
+	if velocity.length() > 1.25:
+		var look_direction = Vector2(velocity.z, velocity.x)
+		%Rat.rotation.y = lerp_angle(%Rat.rotation.y, look_direction.angle(), delta * rotation_speed)
 	
 	if (abs(velocity.x) > 1.0 || abs(velocity.z) > 1.0) && is_on_floor():
 		%Rat.play_walk()
@@ -36,3 +33,6 @@ func _physics_process(delta):
 		%Rat.stop_walk()
 	
 	move_and_slide()
+
+func _process(_delta):
+	%SpringArm3D.position = position
