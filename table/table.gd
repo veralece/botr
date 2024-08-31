081e29df2f15ed13408b87968fc92f7a3a33ec69
Javi <veralece@gmail.com>
2024-08-30 18:01:27 -0700

    added table scene

---
+++ table/table.gd
@@ -0,0 +1,16 @@
+extends StaticBody3D
+
+func store_item(new_item): 
+	var attached_item = %ItemPivot.get_children()
+	if attached_item.size() == 0:
+		new_item.reparent(%ItemPivot)
+		new_item.global_position = %ItemPivot.global_position
+
+func get_item(): 
+	var attached_item = %ItemPivot.get_children()
+	if attached_item.size() > 0:
+		return attached_item[0]
+	
+
+func has_item():
+	return %ItemPivot.get_children().size() > 0
