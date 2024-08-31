463ccf09589099c929323c2e58ba49528a235cd8
Javi <veralece@gmail.com>
2024-08-30 18:01:19 -0700

    changes to rat scene

--- rat/rat.gd
+++ rat/rat.gd
@@ -4,3 +4,18 @@ func play_walk():
 
 func stop_walk():
 	%AnimationPlayer.play("RESET")
+
+func change_item(new_item): 
+	print("change_item called")
+	var attached_item = %ItemPivot.get_children()
+	if attached_item.size() > 0:
+		for child_item in attached_item:
+			child_item.reparent(get_tree().get_root())
+	new_item.reparent(%ItemPivot)
+	new_item.global_position = %ItemPivot.global_position
+
+func remove_item(): 
+	var attached_item = %ItemPivot.get_children()
+	if attached_item.size() > 0:
+		for child_item in attached_item:
+			child_item.reparent(get_tree().get_root())
