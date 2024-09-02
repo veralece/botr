37524d108416873b854259e63a4da196cb2d5c9b
Javi <veralece@gmail.com>
2024-09-02 11:01:56 -0700

    fixed bug with action queue condition preventing array pop

--- player/ActionDetector.gd
+++ player/ActionDetector.gd
@@ -41,14 +41,12 @@ func process_action_exit(exiting_action):
 	if exiting_action_currently_queued:
 		player.action_queue = player.action_queue.filter(func(q_action): return q_action != exiting_action)
 	
-	var current_action_same_as_exiting_action = player.actionable_item == exiting_action
 	if player.actionable_item == exiting_action:
 		player.actionable_item = null
 		%Prompt.text = ""
 	
 	if player.action_queue.size() > 0 && player.actionable_item == null:
-		if player.actionable_item == exiting_action:
-			process_action(player.action_queue.pop_front())
+		process_action(player.action_queue.pop_front())
 
 func _on_area_entered(body):
 	if body.is_in_group("hitboxes"): #hitboxes are usually on a separate child area under the node that stores state
